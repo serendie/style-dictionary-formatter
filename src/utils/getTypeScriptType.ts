9d725dc758d0979b0a79752165486a8b942a3819
nagayama <nagayama@iroirodesign.com>
2025-04-06 19:08:02 +0900

    update regex

--- src/utils/getTypeScriptType.ts
+++ src/utils/getTypeScriptType.ts
@@ -3,7 +3,6 @@ const { getTypeScriptType: _getTypeScriptType } = StyleDictionary.formatHelpers;
 
 export function getTypeScriptType(value: any) {
   const type = _getTypeScriptType(value);
-  // 01や02などがリテラルになっている箇所をクオートする
-  const regex = /(\d+)/g;
-  return type.replace(regex, '"$1"');
+  // 01や02などゼロから始まる数値リテラルになっている箇所をクオートする
+  return type.replace(/\b0\d+\b/g, (match) => `"${match}"`);
 }
