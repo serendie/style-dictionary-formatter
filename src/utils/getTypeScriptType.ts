970997e46ab9d2216846f8b56f305df554678e70
nagayama <nagayama@iroirodesign.com>
2026-02-17 16:31:50 +0900

    ハイフンがついたテーマ名をクォートする

--- src/utils/getTypeScriptType.ts
+++ src/utils/getTypeScriptType.ts
@@ -4,5 +4,8 @@ const { getTypeScriptType: _getTypeScriptType } = StyleDictionary.formatHelpers;
 export function getTypeScriptType(value: any) {
   const type = _getTypeScriptType(value);
   // 01や02などゼロから始まる数値リテラルになっている箇所をクオートする
-  return type.replace(/\b0\d+\b/g, (match) => `"${match}"`);
+  // konjo-darkなどハイフン付きキーもクオートする
+  return type
+    .replace(/\b0\d+\b/g, (match) => `"${match}"`)
+    .replace(/(?<=[{,]\s*)(\w+-\w+)(?=\s*:)/g, (match) => `"${match}"`);
 }
